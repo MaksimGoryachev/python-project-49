4b7d4a8ef4b2ad28a26f7b59cbc43414787958a3
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-04-13 11:08:50 +0500

    Add --group dev flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,6 @@
 import prompt
 
+
 def welcome_user():
     name = prompt.string('May I have yuor name? ')
     print(f'Hello, {name}!')
-    
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,9 +5,8 @@ from ..cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user() # 1-ий Вариант места вызова функции 
-    
-    
+    welcome_user()  # 1-ий Вариант места вызова функции
+
+
 if __name__ == '__main__':
     main()
-    
