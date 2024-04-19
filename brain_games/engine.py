5871b930fb5c3f86e1395318094fd52d59314e4e
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-04-19 12:29:57 +0500

    Moved engine to folder brain_games

---
+++ brain_games/engine.py
@@ -0,0 +1,23 @@
+import prompt
+
+
+def engine_game(question):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(question)
+    correct_answers_count = 0
+    for _ in range(3):
+        right_answer, expression = right_answer_game()
+        print(f"Question: {expression}")
+        geted_answer = input('Your answer: ')
+        if str(right_answer) == str(geted_answer):
+            print('Correct!')
+            correct_answers_count += 1
+            if correct_answers_count == 3:
+                print(f'Congratulations, {name}!')
+        else:
+            print(f'"{geted_answer}" is wrong answer ;(. Correct answer '
+                  f'was "{right_answer}".\n Let\'s try'
+                  f'again, {name}!')
+            break
