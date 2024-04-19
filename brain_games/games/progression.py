880434644353eaee8d1ce9bf270e31f9a3e0c615
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-04-19 21:36:31 +0500

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,16 +6,16 @@ def right_answer_game():
     expressions = []
     for _ in range(3):
         len_progression = randint(5, 10)
-        step_progression = randint(1, 10)
+        step = randint(1, 10)
         start = randint(1, 20)
         number_missing_element = randint(0, len_progression - 1)
         expression = ''
         for i in range(len_progression):
             if i != number_missing_element:
-                expression = expression + str(start + i * step_progression) + " "
+                expression = expression + str(start + i * step) + " "
             else:
                 expression = expression + ".." + " "
-                right_answer = start + i * step_progression
+                right_answer = start + i * step
         expressions.append(expression)
         right_answers.append(right_answer)
     return right_answers, expressions
