b4530a6e102fedf61acfb1e1df65f45d029246b3
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-04-26 09:12:58 +0500

    Update brain_calc,brain_evev, brain_gcd, brain_progression, brain_prime

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-
-
 from brain_games.engine import engine_game
 from brain_games.games.even import generate_data_for_game
 
 
 def main():
-    question, right_answers, expressions = generate_data_for_game()
-    engine_game(question, right_answers, expressions)
+    engine_game(generate_data_for_game)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-
 from ..cli import welcome_user
 
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()  # 1-ий Вариант места вызова функции
+    welcome_user()  
 
 
 if __name__ == '__main__':
